4ef2baa1192bfcb18c8775d7153dd7ff179e5e6c
Raul Costa Junior <raulcostajunior@gmail.com>
2020-04-18 10:29:30 +0200

    Removed placeholder scene from project template.

--- ttPong/GameViewController.swift
+++ ttPong/GameViewController.swift
@@ -15,21 +15,16 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
+        if let skView = self.view as! SKView? {
+            let scene = GameScene(size: skView.bounds.size)
+            scene.scaleMode = .aspectFill
+            skView.presentScene(scene)
+
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            skView.ignoresSiblingOrder = true
         }
+
     }
 
     override var shouldAutorotate: Bool {
