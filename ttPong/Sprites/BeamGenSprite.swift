faf97991779be86fcdc2980b8baf031960d01c3c
Raul Costa Junior <raulcostajunior@gmail.com>
2020-04-19 17:48:51 +0200

    Added some initial sprite files and created folders for sprites and scenes.

---
+++ ttPong/Sprites/BeamGenSprite.swift
@@ -0,0 +1,13 @@
+//
+//  BeamGenSprite.swift
+//  ttPong
+//
+//  Created by Raul Costa Junior on 19.04.20.
+//  Copyright © 2020 Raul Costa Junior. All rights reserved.
+//
+
+import SpriteKit
+
+class BeamGenSprite: SKSpriteNode {
+
+}
---
+++ ttPong/Sprites/DiscSprite.swift
@@ -0,0 +1,13 @@
+//
+//  DiscSprite.swift
+//  ttPong
+//
+//  Created by Raul Costa Junior on 19.04.20.
+//  Copyright © 2020 Raul Costa Junior. All rights reserved.
+//
+
+import SpriteKit
+
+class DiscSprite: SKSpriteNode {
+
+}
